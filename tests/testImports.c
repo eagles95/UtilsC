de1ef11fad0c0a6516dfc335bac8cdb84cba72a6
Joao Dias <jfcdias@sga.pucminas.br>
2017-11-24 12:53:05 -0200

    included import test

---
+++ tests/testImports.c
@@ -0,0 +1,12 @@
+#include<queue.h>
+#include<stack.h>
+#include<pqueue.h>
+#include<list.h>
+#include<hash.h>
+#include<object.h>
+#include<stdio.h>
+
+int main (){
+	printf("If you're seein this, everything was imported successfully\n");
+	return 0;
+}
